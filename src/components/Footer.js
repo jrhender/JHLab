7158679f4d99dbaf4210968497a260e557f45814
jrhender <johnhenderson45@gmail.com>
2017-05-28 18:16:48 -0700

    Modified twitter logo to be blue background

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './../styles/Footer.css';
-import twitterLogo from './../images/Twitter_Logo_White_On_Image.svg';
+//import twitterLogo from './../images/Twitter_Logo_White_On_Image.svg';
+import twitterLogo from './../images/Twitter_Logo_White_On_Blue.svg';
+import linkedinLogo from './../images/In-2CRev-28px-R.png';
 
 const Footer = (props) => {
     return (
@@ -8,6 +10,9 @@ const Footer = (props) => {
             <p>Connect:</p>
             <a href="https://twitter.com/JohnRTHenderson" className='twitter-logo'>
                 <img src={twitterLogo} />
+            </a>
+            <a href="https://www.linkedin.com/in/jhend/" className='linkedin-logo'>
+                <img src={linkedinLogo} />
             </a>
         </div>
     )
